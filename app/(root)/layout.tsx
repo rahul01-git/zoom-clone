372728cd1680e91d362f1051ff8b81d25db02712
Rahul <rahulneupane0090@gmail.com>
2024-04-01 22:24:49 +0545

    sidebar done

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+const RoootLayout = ({ children }: { children: ReactNode }) => {
+  return <main>{children}</main>;
+};
+
+export default RoootLayout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -19,6 +19,13 @@ const config = {
     },
     extend: {
       colors: {
+        dark: {
+          1: "#1c1f2e",
+          2: "#161925",
+        },
+        blue: {
+          1: "#0E78F9",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -75,6 +82,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
