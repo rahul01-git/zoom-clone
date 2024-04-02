e5068aaa1b15f324347f8a5e383b8a5bedbf55f6
Rahul <rahulneupane0090@gmail.com>
2024-04-02 22:13:56 +0545

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href={"/"} className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Yoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/* clerk user mgmt  */}
 
-export default Navbar
+        <MobileNav/>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
