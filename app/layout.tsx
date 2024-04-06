d6a00840f09bcbb2e02a1c9e48063725ba4a291d
Rahul <rahulneupane0090@gmail.com>
2024-04-06 20:18:45 +0545

    meeting room

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-left":
+        return <SpeakerLayout participantsBarPosition={"right"} />;
+      default:
+        return <SpeakerLayout participantsBarPosition={"left"} />;
+    }
+  };
   return (
-    <div>
-      Meeting Room
-    </div>
-  )
-}
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+        <div
+          className={cn("h-[calc(100vh-86px)] hidden ml-2 ", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+          <CallControls/>
+      </div>
+    </section>
+  );
+};
 
-export default MeetingRoom
+export default MeetingRoom;
